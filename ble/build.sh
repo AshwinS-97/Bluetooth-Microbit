c104449ddc8ba2b26ce804388ec49c22ddfd10d5
Ashwin S <120254970+AshwinS-97@users.noreply.github.com>
2023-04-29 17:51:07 +0530

    Delete ble directory

--- ble/build.sh
+++
@@ -1,2 +0,0 @@
-make
-mergehex -m hex/s113_nrf52_7.2.0_softdevice.hex _build/nrf52833_xxaa.hex -o MICROBIT.hex
--- ble/build_lib.sh
+++
@@ -1,6 +0,0 @@
-export PATH=$PATH:/opt/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/bin
-make
-rm -f libble_uart.a
-arm-none-eabi-ar -r libble_uart.a _build/nrf52833_xxaa/*.o
-arm-none-eabi-ar -d libble_uart.a gcc_startup_nrf52833.S.o
-arm-none-eabi-ar -d libble_uart.a system_nrf52833.c.o
